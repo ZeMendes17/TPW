4aa2a1aec1465dcbbdb30220603959ef6284b427
Zé Mendes <mendes.j@ua.pt>
2023-10-06 12:50:17 +0100

    Done aula03 until c), only d) left

--- aula03/eurocv/manage.py
+++ aula03/eurocv/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webproj.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eurocv.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
